6888cf83a733c02f0b0c5d84d815a557544be23b
Efarinyes <efarinyes@gmail.com>
2022-03-22 20:56:27 +0100

    Segon Commit - Començant la lògica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+function AfegirTodo() {
+  const titleRef = useRef();
+  const detailsRef = useRef();
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = titleRef.current.value;
+        const details = detailsRef.current.value;
+
+        title.current.value = "";
+        details.current.value = "";
+      }}
+    >
+      <label>Titol TODO</label>
+      <input ref={titleRef} />
+      <label> Detalls TODO </label>
+      <input ref={detailsRef} />
+      <input type="submit" value="Afegir" />
+    </form>
+  );
+}
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("https://tc-todo-2022.herokuapp.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  });
   return (
     <>
       <header className="App App-header">
         <h3> Gestió de TODO's</h3>
       </header>
       <div className="Container">
-        <h4> Hola Mundo </h4>
+        <h4> Todos </h4>
+        <ul>
+          {todos.map((todo) => (
+            <>
+              <li key={todo.id}>
+                {todo.title} - {todo.details}
+              </li>
+            </>
+          ))}
+        </ul>
+
+        <AfegirTodo />
       </div>
     </>
   );
